01815dc51ef1d64f500dd6ca825008c3773cba0b
Keyon <youngyou.name@gmail.com>
2021-02-24 13:22:46 +0800

    update jwt

--- utils/jwt.test.ts
+++ utils/jwt.test.ts
@@ -1,9 +1,7 @@
-// Copyright 2020 the oak authors. All rights reserved. MIT license.
-
-import { Context } from "../deps.ts";
-import { test, assertEquals, assert } from "../test_deps.ts";
+import { Context, Payload } from "../deps.ts";
+import { assertEquals, test } from "../test_deps.ts";
 import JWT from "./jwt.ts";
- 
+
 const jwt = JWT("DB726D13-9A31-4682-B309-55CE304684C3");
 
 test({
@@ -12,40 +10,57 @@ test({
     const token = await jwt.create({ id: 1 });
     const mockContext = {
       request: {
-        headers: new Headers({ Authorization: `Bearer ${token}` })
+        headers: new Headers({ Authorization: `Bearer ${token}` }),
       },
       state: {},
-      response: {}
+      response: {},
     } as Context;
     const mockNext = () => {
       assertEquals(mockContext.state.jwt.id, 1);
       assertEquals(mockContext.response.status === 401, false);
-      return new Promise<void>(resolve => {
+      return new Promise<void>((resolve) => {
         setTimeout(() => {
           resolve();
         }, 50);
       });
     };
-    await jwt.middleware()(mockContext, mockNext);
-  }
+    await jwt.middleware(
+      async (
+        payload: Payload | null,
+        ctx: Context,
+        next: () => Promise<void>,
+      ) => {
+        if (payload) {
+          ctx.state.jwt = payload;
+          await next();
+        }
+      },
+    )(mockContext, mockNext);
+  },
 });
 test({
   name: "fail",
   async fn() {
     const mockContext = {
       request: {
-        headers: new Headers({ Authorization: "123456" })
+        headers: new Headers({ Authorization: "123456" }),
       },
-      response: {}
+      response: {},
     } as Context;
     const mockNext = () => {
-      return new Promise<void>(resolve => {
+      return new Promise<void>((resolve) => {
         setTimeout(() => {
           resolve();
         }, 50);
       });
     };
-    await jwt.middleware()(mockContext, mockNext);
+    await jwt.middleware((payload: Payload | null, ctx: Context) => {
+      if (payload) {
+        ctx.state.jwt = payload;
+      } else {
+        ctx.response.status = 401
+      }
+    })(mockContext, mockNext);
     assertEquals(mockContext.response.status === 401, true);
-  }
+  },
 });
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,53 +1,42 @@
-import { Payload, Algorithm, jwtCreate, jwtVerify, Middleware, Context } from "../deps.ts";
-
-// deno-lint-ignore no-explicit-any
-type PayloadParse = (payload: Payload) => Record<string, any> | Promise<Record<string, any>>;
-
-function defautParsePayload(payload: Payload) {
-  return { jwt: payload };
-}
-function defaultOnError(ctx: Context, _e: Error) {
-  ctx.response.status = 401;
-  return;
-}
+import {
+  Algorithm,
+  Context,
+  jwtCreate,
+  jwtVerify,
+  Middleware,
+  Payload,
+} from "../deps.ts";
 
 function jwt(secret: string, alg: Algorithm = "HS512") {
   async function create(data: Payload) {
     return await jwtCreate({ alg, typ: "JWT" }, data, secret);
   }
-  async function parse(token: string) {
+  async function parse<T extends Payload = Payload>(token: string) {
     const text = token.startsWith("Bearer ") ? token.slice(7) : token;
-    return await jwtVerify(text, secret, alg);
+    const payload = await jwtVerify(text, secret, alg);
+    return payload as T;
   }
 
-  function middleware(parsePayload: PayloadParse = defautParsePayload, onError = defaultOnError): Middleware {
-    return async function (ctx, next) {
+  function middleware<T extends Payload = Payload>(
+    fun: (payload: T | null, ctx: Context, next: () => Promise<void>) => void,
+  ): Middleware {
+    return async function (ctx: Context, next: () => Promise<void>) {
       const authHeader = ctx.request.headers.get("Authorization")!;
-      if (!authHeader || !authHeader.startsWith("Bearer ") || authHeader.length <= 7) {
-        onError(ctx, new Error("JWT not found"));
-        return;
+      let payload = null;
+      if (authHeader && authHeader.startsWith("Bearer ")) {
+        try {
+          payload = await parse<T>(authHeader);
+        } catch (e) {
+          console.log(e);
+        }
       }
-      try {
-        const payload = await parse(authHeader);
-        const data = await parsePayload(payload);
-        const keys = Object.keys(data);
-        keys.forEach(k => {
-          ctx.state[k] = data[k];
-        });
-        await next();
-        keys.forEach(k => {
-          delete ctx.state[k];
-        });
-      } catch (e) {
-        onError(ctx, e);
-        return;
-      }
+      await fun(payload, ctx, next);
     };
   }
   return {
     create,
     parse,
-    middleware
+    middleware,
   };
 }
 
