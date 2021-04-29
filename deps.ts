5e5c0472b1a7d9ff656a8f7556372bd855e5be68
Keyon U <youngyou.name@gmail.com>
2021-04-29 15:52:57 +0800

    Update oak version to v7.3.0

--- deps.ts
+++ deps.ts
@@ -1,17 +1,11 @@
 // jwt
-export {
-  create as jwtCreate,
-  verify as jwtVerify,
-} from "https://deno.land/x/djwt@v2.2/mod.ts";
+export { create as jwtCreate, verify as jwtVerify } from "https://deno.land/x/djwt@v2.2/mod.ts";
 export type { Payload } from "https://deno.land/x/djwt@v2.2/mod.ts";
 export type { Algorithm } from "https://deno.land/x/djwt@v2.2/algorithm.ts";
 
 // xak
-export type {
-  Middleware,
-  RouterMiddleware,
-} from "https://deno.land/x/oak@v6.5.0/mod.ts";
-export { Context } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export type { Middleware, RouterMiddleware } from "https://deno.land/x/oak@v7.3.0/mod.ts";
+export { Context } from "https://deno.land/x/oak@v7.3.0/mod.ts";
 
 export { Sha1 } from "https://deno.land/std@0.82.0/hash/sha1.ts";
 export { decode } from "https://deno.land/std@0.82.0/encoding/base64.ts";
